4583e66fb4201cb0e743230cbdb48d334abba4b7
wjiscoding <155940891+wjiscoding@users.noreply.github.com>
2024-01-17 11:43:19 -0800

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
-function purchase() {
-    console.log("You are rich!");
+function onYolo() {
+    console.log('on Yolo')
+    dataLayer.push({'event': 'purchase', "eventModel":{"currency": "USD"}}) // to trigger Tag-yolo_shopping
+    document.getElementById('modal-yolo').style.display='block'
+}
+
+function onInitCheckout() {
+    alert('init checkout!!!!')
+}
+
+function onContact() {
+    console.log('on Contact')
+    document.getElementById('modal-contact').style.display='block'
+}
+
+function onMenu() {
+    console.log('on Menu')
+    document.getElementById('modal-menu').style.display='block'
 }
